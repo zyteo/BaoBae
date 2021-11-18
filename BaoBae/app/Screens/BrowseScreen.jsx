9176e4477df747a018844a87bbc05deff31604de
Stabilo <zteo015@e.ntu.edu.sg>
2021-11-18 16:18:44 +0800

    Styling browse screen - done the search bar

--- BaoBae/app/Screens/BrowseScreen.jsx
+++ BaoBae/app/Screens/BrowseScreen.jsx
@@ -9,8 +9,84 @@ import {
   TouchableOpacity,
   ScrollView,
 } from "react-native";
+import styled from "styled-components/native";
 import { getCurrentUser, getItems, logOutUser } from "../../firebase";
 import colours from "../Config/colours";
+
+const StyledView = styled.View`
+  flex: 1;
+  background-color: ${colours.primary};
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+`;
+
+const StyledSearchView = styled.View`
+  flex: 1;
+  background-color: ${colours.primary};
+  align-items: center;
+  justify-content: center;
+  width: 80%;
+  flexDirection: row;
+`;
+
+const StyledTextInput = styled.TextInput`
+  height: 40px;
+  margin: 2px;
+  width: 95%;
+  borderWidth: 1.5px;
+  borderRadius: 8px;
+  padding: 8px;
+  background-color: ${colours.inputbox};
+  color: ${colours.inputboxtext};
+  borderColor: ${colours.border};
+  fontSize: 14px;
+  `;
+
+const StyledTouchableOpacity = styled.TouchableOpacity`
+  height: 40px;
+  margin: 2px;
+  borderWidth: 0.5px;
+  borderRadius: 6px;
+  padding: 8px;
+  background-color: ${colours.buttonbox};
+  color: ${colours.buttonboxtext};
+  borderColor: ${colours.border};
+  `;
+
+const StyledSearch = styled(StyledTouchableOpacity)`
+  height: 40px;
+`;
+
+const StyledTouchableOpacityText = styled.Text`
+  color: ${colours.buttonboxtext};
+  fontSize: 17px;
+  `;
+
+const StyledImage = styled.Image`
+  width: 80px;
+  height: 80px;
+  borderRadius: 6px;
+  `;
+
+const StyledText = styled.Text`
+  height: 20px;
+  margin: 16px;
+  color: ${colours.inputboxtext};
+  fontSize: 16px;
+  align-items: center;
+  justify-content: center;
+`;
+
+const StyledViewItems = styled.View`
+  flex: 1;
+  background-color: ${colours.primary};
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  flexDirection: row;
+  flexWrap: wrap;
+`;
 
 const styles = StyleSheet.create({
   container: {
@@ -57,47 +133,46 @@ const BrowseScreen = ({ route, navigation }) => {
   return (
     <>
       <ScrollView>
-        <View style={styles.container}>
-          <View>
-            <Text>What are you looking for today, {user.username}?</Text>
-            <TextInput
-              style={styles.input}
+        <StyledView>
+          <Text>What are you looking for today, {user.username}?</Text>
+          <StyledSearchView>
+            <StyledTextInput
               placeholder="Search items"
               onChangeText={(text) => setText(text)}
             />
-            <Button
-              title="🔍"
+            <StyledSearch
               onPress={() =>
                 navigation.push("Search", {
                   email: user.email,
                   search: text,
                 })
               }
-            />
-          </View>
+            >
+              <StyledTouchableOpacityText>🔍</StyledTouchableOpacityText>
+            </StyledSearch>
+          </StyledSearchView>
 
           <View style={styles.items}>
             {items.map((element, index) => {
               return (
-                
-                  <View key={index}>
-                    <Text>{element.name}</Text>
-                    <TouchableOpacity
-                      onPress={() =>
-                        navigation.push("Item", {
-                          name: element.name,
-                          email: user.email,
-                        })
-                      }
-                    >
-                      <Image
-                        style={styles.photo}
-                        source={{
-                          uri: element.image,
-                        }}
-                      />
-                    </TouchableOpacity>
-                  </View>
+                <View key={index}>
+                  <Text>{element.name}</Text>
+                  <TouchableOpacity
+                    onPress={() =>
+                      navigation.push("Item", {
+                        name: element.name,
+                        email: user.email,
+                      })
+                    }
+                  >
+                    <Image
+                      style={styles.photo}
+                      source={{
+                        uri: element.image,
+                      }}
+                    />
+                  </TouchableOpacity>
+                </View>
               );
             })}
           </View>
@@ -109,7 +184,7 @@ const BrowseScreen = ({ route, navigation }) => {
               onPress={() => navigation.push("Cart", { email: user.email })}
             />
           </View>
-        </View>
+        </StyledView>
       </ScrollView>
     </>
   );
